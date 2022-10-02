4b87732b2783f43a7b741c77e6e21445ffc4acf2
Angelo <Angelodimauro91@gmail.com>
2022-10-02 22:37:35 +0200

    import vecchio progetto

--- boolflix/src/App.vue
+++ boolflix/src/App.vue
@@ -1,19 +1,47 @@
 <template>
   <div id="app">
-    <h1>cisao</h1>
+    <div>
+      <input type="text" v-model="query"/> <button @click="search">Cerca</button>
+    </div>
   </div>
 </template>
 
 <script>
-
+  import axios from "axios";
 
 export default {
   name: 'App',
-  components: {
-    
-  }
+  data(){
+    return {
+      query: ``,
+      movies: []
+    }
+  },
+  methods:{
+    search(){
+      this.queryApi(this.query);
+    },
+    queryApi(textToSearch){
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c4843b621764220065cb747f4ce24763&query=${textToSearch}&language=it-IT`)
+        .then((response)=>{
+          console.log(response);
+          if (response.status === 200){
+            this.movies = response.data.results;
+        }
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+    }
+  },
 }
 </script>
 
+
+
+
+
+
 <style lang="scss">
-</style>
+
+</style> 
