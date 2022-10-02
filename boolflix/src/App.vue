6554fe1a454603d964e040157b2dcf5b90ae0fc9
Angelo <Angelodimauro91@gmail.com>
2022-10-02 22:27:32 +0200

    restart

---
+++ boolflix/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>ciao</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
